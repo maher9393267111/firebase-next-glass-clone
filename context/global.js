897a36a920a78a054f2c4176c8b2e892e726f56d
maher <gomemahero.com>
2022-06-16 14:43:20 +0300

    find products have price 323 or price 277

--- context/global.js
+++ context/global.js
@@ -192,7 +192,7 @@ const AuthContext = ({ children }) => {
 
 
     const productsref   = collection(db,'products')
-    const q = query(productsref, where('name', 'in', ['ALCONty', 'katen4']));
+    const q = query(productsref, where('price', 'in', ['323', '277']));
 
     
 
